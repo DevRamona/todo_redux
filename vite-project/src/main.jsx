31ea3964d562ef3c378bf763c09759566c1ae561
DevRamona <r.ingabire@alustudent.com>
2024-08-14 16:42:15 +0200

    Configure store component

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
+import { Provider } from "react-redux";
+import store from "./Store/store.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}>
     
-    <App />
-    
-    
-  </StrictMode>,
-)
+      <App />
+    </Provider>
+  </StrictMode>
+);
