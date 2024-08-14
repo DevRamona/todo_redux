60715aef7fefe20b3712a5ac0bdc878a4f3c88dc
DevRamona <r.ingabire@alustudent.com>
2024-08-14 16:24:36 +0200

    createSlice

---
+++ vite-project/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-blue-100 text-5xl font-bold text-center">todos</h1>
+    </div>
+  );
+}
+
+export default Header;
