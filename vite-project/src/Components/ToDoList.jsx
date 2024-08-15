147cdd5bc1a8115ba84326c7688b5e76286e813a
DevRamona <r.ingabire@alustudent.com>
2024-08-15 21:49:20 +0200

    initial changes

--- vite-project/src/Components/ToDoList.jsx
+++ vite-project/src/Components/ToDoList.jsx
@@ -12,11 +12,23 @@ const ToDoList = () => {
     const todoList = useSelector((state) => state.todo.todoList)
     const sortCriteria = useSelector((state) => state.todo.sortCriteria)
     const [currentToDo, setCurrentToDo] = useState(null)
+    const [newTask, setNewTask] = useState("")
   return (
     <div>
-        <div>
-        <input type='text' placeholder={currentToDo ? "Update your tasks" : "Enter your task"}/>
+        <div className='flex justify-between'>
+        <input type='text' value={newTask} placeholder={currentToDo ? "Update your tasks" : "Enter your task"}/>
 
+        </div>
+        <div>
+          {currentToDo ? (
+            <>
+            <button>Save</button>
+            <button>Cancel</button>
+            </>
+          ) : (
+            <> <button className='bg-black border px-4 py-1 text-white '>Cancel</button>
+            <button className='bg-orange-700 border px-4 py-1 text-white'>Add</button> </>
+          )}
         </div>
         <button className='bg-orange-600 text-center text-white rounded-md px-10 py-3 mt-7'>Add task</button>
       
