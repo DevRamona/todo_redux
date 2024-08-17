ab3821f2e6976344e88277d1e07d8354c65b1eb1
DevRamona <r.ingabire@alustudent.com>
2024-08-17 22:12:57 +0200

    final updates

--- vite-project/src/Components/ToDoList.jsx
+++ vite-project/src/Components/ToDoList.jsx
@@ -54,7 +54,7 @@ const ToDoList = () => {
           <>
             <button
               onClick={handleAddToDo}
-              className="bg-green-500 text-white px-4 py-2 rounded mr-2"
+              className="bg-green-600 text-white px-4 py-2 rounded mr-2"
             >
               Save
             </button>
